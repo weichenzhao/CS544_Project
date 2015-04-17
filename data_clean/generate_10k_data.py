d1049d26a04ee53083e0a8c1e1c743a3b685a40f
richard zhao <09zwcbupt@gmail.com>
2015-04-16 23:54:19 -0700

    update

--- data_clean/generate_10k_data.py
+++ data_clean/generate_10k_data.py
@@ -5,7 +5,8 @@ import random
 
 start = time.time()
 standard = 10000
-data = open('dataset.txt', 'r')
+test_standard = 1000
+data = open('2m_dataset.txt', 'r')
 data_dict = json.load(data)
 print "loading data used", time.time() - start, "seconds"
 
@@ -13,6 +14,9 @@ new_data = {}
 tot_count = 0
 keys = data_dict.keys()
 
+"""
+    10k data
+"""
 # select 6k file, evenly for each key
 for key in keys:
     new_data[key] = {}
@@ -24,7 +28,7 @@ for key in keys:
         if ( not tot_count % 1000 ) and tot_count > 0 :
             print "progress: ", tot_count * 100 /standard, "%", tot_count
 
-# select 4k file, randomly
+# select 4k part, randomly
 while tot_count < standard:
     # select a key randomly 
     key = keys[random.randint(0,1999)]
@@ -39,3 +43,23 @@ while tot_count < standard:
 print(len(new_data))
 json.dump(new_data, open('10k_data.txt', 'w'))
 #pdb.set_trace()
+
+"""
+    1k test data
+"""
+# generate 1k test file
+test_data = {}
+tot_count = 0
+while tot_count < test_standard:
+    # select a key randomly 
+    key = keys[random.randint(0,1999)]
+    # select a random index based on key
+    index = data_dict[key].keys()[random.randint(0, len(data_dict[key].keys()) - 1)]
+    test_data[index] = data_dict[key][index]
+    tot_count += 1
+    data_dict[key].pop(index)
+    if ( not tot_count % 100 ) and tot_count > 0 :
+        print "progress: ", tot_count * 100 /test_standard, "%", tot_count
+
+print(len(test_data))
+json.dump(test_data, open('1k_test_data.txt', 'w'))
--- data_clean/generate_test_set_2000_tags.py
+++ data_clean/generate_test_set_2000_tags.py
@@ -1,9 +1,11 @@
 import os
 import pdb
 import json
+import time
+import random
 
 input_name = "/Users/apple/Desktop/USC/CS544 NLP/project/data/"
-output_name = "dataset.txt"
+output_name = "2m_dataset.txt"
 standard = 1000 * 2000.0
 
 # get the 2000 tags
@@ -14,7 +16,8 @@ for tag in tag_file:
 #print file_count
 
 # get filename in input dir
-files = set(os.listdir(input_name))
+files = list(os.listdir(input_name))
+random.shuffle(files)
     
 """
     get 1000 file for each tag
@@ -22,17 +25,41 @@ files = set(os.listdir(input_name))
         { tag : { 'id': file entry } }
 """
 data_set = {}
+chosen_id = {}
 tot_count = 0
+file_cnt = 0
+round_ = 0
+
 for data_file in files:
-    print "processing", data_file
+    file_cnt += 1
+    print "processing", data_file, file_cnt
     fin = open(input_name + data_file, 'r')
     data_dict = json.load(fin)
+    fin.close()
     for key in data_dict.keys():
-        # 0-title, 1-body, 2-code, 3-tags
-        tags = data_dict[key][3].split()
-        for t in tags:
-            if t in file_count:
+        if key not in chosen_id:
+            # 0-title, 1-body, 2-code, 3-tags
+            """
+                popular tags tends to appear at first, if put these files
+                into the tag dict, then it is possible that less frequent
+                tags are ignored and therefore those tags might not get
+                enough file. Reverse the order might do the work
+            """
+            tags = data_dict[key][3].split()
+            tags.reverse()
+            tag_list = []
+            """
+                have to consider all tags, and select the one currently have
+                the least number of file append into the dataset
+            """
+            for t in tags:
+                if t in file_count:
+                    tag_list.append([file_count[t], t])
+
+            if not tag_list == []:
+                tag_list.sort()
                 tot_count += 1
+                t = tag_list[0][1]
                 file_count[t] += 1
                 # have this tag in data set, add into file dict of this tag
                 if t not in data_set:
@@ -43,9 +70,20 @@ for data_file in files:
                     file_count.pop(t)
                 if ( not tot_count % 10000 ) and tot_count > 0:
                     print "progress: ", tot_count * 100 /standard, "%", tot_count
+                # already count this doc once. should not count it any more
+                chosen_id[key] = True
+
     if len(file_count) == 0:
         break
 
 print(len(data_set))
+print(tot_count)
+detail = open('dataset.detail.txt', 'w')
+for key in data_set.keys():
+    detail.write("key:" + key + " " + str(len(data_set[key].keys())))
+    if len(data_set[key].keys()) < 1000:
+        print len(data_set[key].keys())
 #pdb.set_trace()
+start = time.time()
 json.dump(data_set, open(output_name, 'w'))
+print "writting data used", time.time() - start, "seconds"
